61745685c9bbbd66fe366b3e62b522e84da5e970
Kalman Hazins <kalmanh@gmail.com>
2018-10-18 10:31:04 -0400

    Updated with async/await instead

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ const fs = require('fs-extra')
 // REPLACE THIS LINE WITH THE DIRECTORY WHERE YOUR GOOGLE PHOTOS LIVE
 const googlePhotosDir = '/Users/kalmanhazins/Google Drive/Google Photos'
 
-fs.readdir( googlePhotosDir )
-.then( items => {
-    const picsOrMovies = items.filter(name => name.match(/20\d{6}_\d{6}.*\.(jpg|mp4)/))
-
-    picsOrMovies.forEach( item => {
+const organizeMedia = async () => {
+    const items = await fs.readdir(googlePhotosDir);
+    const picsOrMovies = items.filter(name => name.match(/20\d{6}_\d{6}.*\.(jpg|mp4)/));
+    picsOrMovies.forEach(async item => {
         const [, year, month, day] = /(\d{4})(\d{2})(\d{2})/.exec(item) || []
-
         if(year && month && day){
             const destDir = `${googlePhotosDir}/${year}/${month}/${day}`;
-            fs.ensureDir(destDir)
-            .then(() => {
-                fs.move(`${googlePhotosDir}/${item}`, `${destDir}/${item}`)
-            })
-            .catch(err => {
-                console.error(err)
-            })
+            await fs.ensureDir(destDir);
+            await fs.move(`${googlePhotosDir}/${item}`, `${destDir}/${item}`, { overwrite: true });
         }
     });
-})
+};
 
+try {
+    organizeMedia();
+}
+catch(error){
+    console.error(error);
+}
 
